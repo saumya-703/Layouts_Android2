af8767b2e75fc6bdea83dade0a7297b72ab98eaf
saumya <verma.saumya.703@gmail.com>
2023-06-21 15:35:06 +0530

    Changes-2

--- app/src/main/java/com/project/layouts/MainActivity.java
+++ app/src/main/java/com/project/layouts/MainActivity.java
@@ -3,12 +3,28 @@ package com.project.layouts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    RadioButton pb,gs,lb;
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pb=findViewById(R.id.pb);
+        gs=findViewById(R.id.gs);
+        lb=findViewById(R.id.lb);
+        submit=findViewById(R.id.submit);
+
+        show();
+    }
+
+    public void show() {
+        submit.setOnClickListener((view -> {
+            int k= pb.getChecke
+        }));
     }
 }
